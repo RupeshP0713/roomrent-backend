22b62dcfdb48c3b4a96f514b510b916a06b0dee4
Patrarupesh9157 <patrarupesh9157@gmail.com>
2026-02-18 00:25:10 +0530

    update

--- models/Message.js
+++ models/Message.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const MessageSchema = new mongoose.Schema({
     senderId: { type: String, required: true },
-    senderRole: { type: String, enum: ['Malik', 'Bhadot', 'admin'], required: true },
+    senderRole: { type: String, enum: ['Malik', 'Bhadot', 'admin', 'malik', 'bhadot'], required: true },
     receiverId: { type: String, required: true }, // 'admin' or user ID
-    receiverRole: { type: String, enum: ['Malik', 'Bhadot', 'admin'], required: true },
+    receiverRole: { type: String, enum: ['Malik', 'Bhadot', 'admin', 'malik', 'bhadot'], required: true },
     content: { type: String, required: true },
     isRead: { type: Boolean, default: false },
     timestamp: { type: Date, default: Date.now }
