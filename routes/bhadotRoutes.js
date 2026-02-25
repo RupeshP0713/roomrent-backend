dad88ae62a40112c7046c2b6b063fd49571e9549
Patrarupesh9157 <patrarupesh9157@gmail.com>
2026-02-25 23:14:33 +0530

    Update 2

--- routes/bhadotRoutes.js
+++ routes/bhadotRoutes.js
@@ -9,10 +9,10 @@ router.post('/register', bhadotController.registerBhadot);
 router.post('/login', bhadotController.loginBhadot);
 
 router.get('/available-rooms', bhadotController.getAvailableRooms); // Public
-router.get('/:id', authMiddleware, roleMiddleware(['bhadot']), bhadotController.getBhadotById);
-router.put('/:id', authMiddleware, roleMiddleware(['bhadot']), bhadotController.updateBhadot);
+router.get('/:id', authMiddleware, roleMiddleware(['bhadot', 'admin']), bhadotController.getBhadotById);
+router.put('/:id', authMiddleware, roleMiddleware(['bhadot', 'admin']), bhadotController.updateBhadot);
 router.put('/:id/active', authMiddleware, roleMiddleware(['bhadot']), bhadotController.toggleBhadotActive);
-router.get('/:id/requests', authMiddleware, roleMiddleware(['bhadot']), bhadotController.getBhadotRequests);
+router.get('/:id/requests', authMiddleware, roleMiddleware(['bhadot', 'admin']), bhadotController.getBhadotRequests);
 router.put('/request/:requestId', authMiddleware, roleMiddleware(['bhadot']), bhadotController.updateRentRequestStatus);
 
 module.exports = router;
--- routes/malikRoutes.js
+++ routes/malikRoutes.js
@@ -8,12 +8,12 @@ const roleMiddleware = require('../middleware/roleMiddleware');
 router.post('/register', malikController.registerMalik);
 router.post('/login', malikController.loginMalik);
 
-router.get('/:id', authMiddleware, roleMiddleware(['malik']), malikController.getMalikById);
+router.get('/:id', authMiddleware, roleMiddleware(['malik', 'admin']), malikController.getMalikById);
 router.put('/:id/address', authMiddleware, roleMiddleware(['malik']), malikController.updateMalikAddress);
-router.put('/:id', authMiddleware, roleMiddleware(['malik']), malikController.updateMalik);
+router.put('/:id', authMiddleware, roleMiddleware(['malik', 'admin']), malikController.updateMalik);
 router.get('/:id/bhadots', authMiddleware, roleMiddleware(['malik']), malikController.getMalikBhadots);
 router.post('/request', authMiddleware, roleMiddleware(['malik']), malikController.createRentRequest);
-router.get('/:id/requests', authMiddleware, roleMiddleware(['malik']), malikController.getMalikRequests);
+router.get('/:id/requests', authMiddleware, roleMiddleware(['malik', 'admin']), malikController.getMalikRequests);
 
 module.exports = router;
 
